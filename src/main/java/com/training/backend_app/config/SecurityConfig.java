a7ecba50bb61ed49b0bda3d6f4305ef9fe0f7f89
Tanuja Randave <randavetanuja@gmail.com>
2026-03-21 00:05:19 +0530

    new url added

--- src/main/java/com/training/backend_app/config/SecurityConfig.java
+++ src/main/java/com/training/backend_app/config/SecurityConfig.java
@@ -64,7 +64,7 @@ public class SecurityConfig {
                         "http://localhost:3001",
                         "http://localhost:5173",
                         "https://*.vercel.app",
-                        "https://frontend-app-z5g5.vercel.app"));
+                        "https://frontend-app123-delta.vercel.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
